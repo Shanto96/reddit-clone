5a4d2662956ff4d338109f80d1160b420d999d01
kibria <shan2neo@gmail.com>
2023-01-28 18:32:11 +0600

    --done navbar search bar design

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,12 @@ import * as React from 'react'
 import {  ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import {theme} from '../chakara/theme'
-
+import Layout from '../Components/Layouts/Layout'
 export default function App({ Component, pageProps }: AppProps) {
-  return <ChakraProvider theme={theme}> 
-  <Component {...pageProps} />
+  return <ChakraProvider theme={theme}>
+    <Layout >
+    <Component {...pageProps} />
+      </Layout> 
+  
     </ChakraProvider>
 }
